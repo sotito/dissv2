78bf655d725870789dfc54e1fa3a0155af5a6cc9
sotito <christian.soto9@upr.edu>
2015-10-13 00:40:40 +0000

    m

---
+++ public/modules/services/services/services.client.service.js
@@ -0,0 +1,15 @@
+'use strict';
+
+angular.module('services').factory('Services', [
+	function() {
+		// Services service logic
+		// ...
+
+		// Public API
+		return {
+			someMethod: function() {
+				return true;
+			}
+		};
+	}
+]);
